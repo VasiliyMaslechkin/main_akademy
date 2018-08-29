1d7609f52c50635bc37480ea57e86e5e0b107c52
Maslechkin Vasiliy <42152079+VasiliyMaslechkin@users.noreply.github.com>
2018-08-29 12:05:19 +0300

    Delete 08-22-01.php

--- 08-22-01.php
+++
@@ -1,8 +0,0 @@
-<?php
-//У: число передается параметром.
-//З: сделайте функцию, которая возвращает квадрат числа.
-function quareOfNumber ($num) {
-    return $num * $num;
-}
-$number = mt_rand(1,256);
-echo quareOfNumber ($number);
